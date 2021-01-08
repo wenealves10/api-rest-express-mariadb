a38ad64559cab3c3f3cb068669c615c9a78fe199
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-08 10:09:15 -0300

    modifying the code

--- src/controllers/HomeControllers.js
+++ src/controllers/HomeControllers.js
@@ -1,18 +1,8 @@
 // eslint-disable-next-line no-unused-vars
-import Student from '../models/Student';
-
 class HomeControllers {
   async index(req, res) {
-    const student = await Student.create({
-      name: 'Wene',
-      surname: 'Alves de Oliveira',
-      email: 'wene.alves@gmail.com',
-      age: 19,
-      height: 1.68,
-      weight: 88,
-    });
     res.status(200).json({
-      student,
+      status: 'ok',
     });
   }
 }
