d900969e80bb4ab6b092bd1853e8d572ed16de84
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-08 16:50:55 -0300

    Report Model

--- src/models/Report.js
+++ src/models/Report.js
@@ -3,12 +3,104 @@ import Sequelize, { Model } from 'sequelize';
 export default class Report extends Model {
   static init(sequelize) {
     super.init({
-      matter: Sequelize.STRING,
-      note_1: Sequelize.FLOAT,
-      note_2: Sequelize.FLOAT,
-      note_3: Sequelize.FLOAT,
-      note_4: Sequelize.FLOAT,
-      average: Sequelize.FLOAT,
+      matter: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [4, 255],
+            msg: 'the surname must be between 4 and 255 characters.',
+          },
+          isAlpha: {
+            msg: 'Only letters',
+          },
+        },
+      },
+      note_1: {
+        type: Sequelize.FLOAT,
+        defaultValue: 0,
+        validate: {
+          isNumeric: {
+            msg: 'whole numbers only.',
+          },
+          max: {
+            args: 10,
+            msg: 'Only grades less than or equal to 10.',
+          },
+          min: {
+            args: 0,
+            msg: 'Only grades equal to or greater than 0',
+          },
+        },
+      },
+      note_2: {
+        type: Sequelize.FLOAT,
+        defaultValue: 0,
+        validate: {
+          isNumeric: {
+            msg: 'whole numbers only.',
+          },
+          max: {
+            args: 10,
+            msg: 'Only grades less than or equal to 10.',
+          },
+          min: {
+            args: 0,
+            msg: 'Only grades equal to or greater than 0',
+          },
+        },
+      },
+      note_3: {
+        type: Sequelize.FLOAT,
+        defaultValue: 0,
+        validate: {
+          isNumeric: {
+            msg: 'whole numbers only.',
+          },
+          max: {
+            args: 10,
+            msg: 'Only grades less than or equal to 10.',
+          },
+          min: {
+            args: 0,
+            msg: 'Only grades equal to or greater than 0',
+          },
+        },
+      },
+      note_4: {
+        type: Sequelize.FLOAT,
+        defaultValue: 0,
+        validate: {
+          isNumeric: {
+            msg: 'whole numbers only.',
+          },
+          max: {
+            args: 10,
+            msg: 'Only grades less than or equal to 10.',
+          },
+          min: {
+            args: 0,
+            msg: 'Only grades equal to or greater than 0',
+          },
+        },
+      },
+      average: {
+        type: Sequelize.FLOAT,
+        defaultValue: 0,
+        validate: {
+          isNumeric: {
+            msg: 'whole numbers only.',
+          },
+          max: {
+            args: 10,
+            msg: 'Only grades less than or equal to 10.',
+          },
+          min: {
+            args: 0,
+            msg: 'Only grades equal to or greater than 0',
+          },
+        },
+      },
     }, {
       sequelize,
     });
