4a806adda4dfb320151f884d210b9f9ae00702dc
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-08 10:10:02 -0300

    migration students_reports

---
+++ src/database/migrations/20210108115754-student_report.js
@@ -0,0 +1,48 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('students_reports', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      matter: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      note_1: {
+        type: Sequelize.FLOAT,
+        allowNull: true,
+      },
+      note_2: {
+        type: Sequelize.FLOAT,
+        allowNull: true,
+      },
+      note_3: {
+        type: Sequelize.FLOAT,
+        allowNull: true,
+      },
+      note_4: {
+        type: Sequelize.FLOAT,
+        allowNull: true,
+      },
+      average: {
+        type: Sequelize.FLOAT,
+        allowNull: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('students_reports');
+  },
+};
