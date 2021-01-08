7d4a099e0c85e9808d11c3689e4bdfb20d22c032
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-08 12:11:08 -0300

    User Model validate

--- src/models/User.js
+++ src/models/User.js
@@ -1,13 +1,51 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
 
 export default class User extends Model {
   static init(sequelize) {
     super.init({
-      name: Sequelize.STRING,
-      email: Sequelize.STRING,
-      password: Sequelize.STRING,
+      name: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [4, 255],
+            msg: 'the name must be between 4 and 255 characters.',
+          },
+        },
+      },
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        unique: {
+          msg: 'Email already registered.',
+        },
+        validate: {
+          isEmail: {
+            msg: 'Invalid email.',
+          },
+        },
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+      },
+      password: {
+        type: Sequelize.VIRTUAL,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [6, 50],
+            msg: 'Password must be between 6 and 50 characters.',
+          },
+        },
+      },
+
     }, {
       sequelize,
+    });
+    this.addHook('beforeSave', async (user) => {
+      user.password_hash = await bcrypt.hash(user.password, 8);
     });
     return this;
   }
