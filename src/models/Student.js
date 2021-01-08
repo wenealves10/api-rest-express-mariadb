801b09752192736788c5b01b90d8e6341a618f3c
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-08 12:44:58 -0300

    modifying the code

--- src/models/Student.js
+++ src/models/Student.js
@@ -3,9 +3,38 @@ import Sequelize, { Model } from 'sequelize';
 export default class Student extends Model {
   static init(sequelize) {
     super.init({
-      name: Sequelize.STRING,
-      surname: Sequelize.STRING,
-      email: Sequelize.STRING,
+      name: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [4, 16],
+            msg: 'the name must be between 4 and 16 characters.',
+          },
+        },
+      },
+      surname: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [4, 255],
+            msg: 'the surname must be between 4 and 255 characters.',
+          },
+        },
+      },
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        unique: {
+          msg: 'Email already registered.',
+        },
+        validate: {
+          isEmail: {
+            msg: 'Invalid email.',
+          },
+        },
+      },
       age: Sequelize.STRING,
       height: Sequelize.STRING,
       weight: Sequelize.STRING,
