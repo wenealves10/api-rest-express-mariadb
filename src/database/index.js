30a4412f1018e8af717778b18ab3e475e237b9c3
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-08 10:22:30 -0300

    importing model user

--- src/database/index.js
+++ src/database/index.js
@@ -2,8 +2,9 @@ import Sequelize from 'sequelize';
 import databaseConfig from '../config/database';
 import Student from '../models/Student';
 import Report from '../models/Report';
+import User from '../models/User';
 
-const models = [Student, Report];
+const models = [Student, Report, User];
 
 const connection = new Sequelize(databaseConfig);
 
