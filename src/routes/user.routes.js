6e79d9a5363e6d2c7ad4590c8f9200fcecc4c052
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-08 20:33:12 -0300

    routes users

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,6 +3,10 @@ import UserControllers from '../controllers/UserControllers';
 
 const routes = new Router();
 
+routes.get('/', UserControllers.index);
+routes.get('/:id', UserControllers.show);
 routes.post('/', UserControllers.create);
+routes.put('/:id', UserControllers.update);
+routes.delete('/:id', UserControllers.delete);
 
 export default routes;
