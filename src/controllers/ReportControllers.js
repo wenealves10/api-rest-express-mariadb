7514e04693f9b69703aefce141a17a9168d6b369
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-08 17:49:06 -0300

    routes report

---
+++ src/controllers/ReportControllers.js
@@ -0,0 +1,15 @@
+import Report from '../models/Report';
+
+class ReportControllers {
+  async create(req, res) {
+    try {
+      const report = await Report.create(req.body);
+      res.status(200).json({ report });
+    } catch (e) {
+      // res.status(400).json({ error: e.errors.map((err) => err.message) });
+      res.status(400).json({ e });
+    }
+  }
+}
+
+export default new ReportControllers();
