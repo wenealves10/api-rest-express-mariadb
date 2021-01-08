381c69f64dfe53148c6e8351157783c781d538b3
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-08 10:29:47 -0300

    modifying migration

--- src/database/migrations/20210108131154-users.js
+++ src/database/migrations/20210108131154-users.js
@@ -14,8 +14,9 @@ module.exports = {
       email: {
         type: Sequelize.STRING,
         allowNull: false,
+        unique: true,
       },
-      password: {
+      password_hash: {
         type: Sequelize.STRING,
         allowNull: false,
       },
