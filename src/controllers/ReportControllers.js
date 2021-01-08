febc23929ab89e181e907334abde2c41796cb742
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-08 18:04:39 -0300

    modifying the code

--- src/controllers/ReportControllers.js
+++ src/controllers/ReportControllers.js
@@ -6,8 +6,7 @@ class ReportControllers {
       const report = await Report.create(req.body);
       res.status(200).json({ report });
     } catch (e) {
-      // res.status(400).json({ error: e.errors.map((err) => err.message) });
-      res.status(400).json({ e });
+      res.status(400).json({ error: e.errors.map((err) => err.message) });
     }
   }
 }
