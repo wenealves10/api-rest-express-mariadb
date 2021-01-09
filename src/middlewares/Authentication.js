fc1dd95a8170e9ecd347de7fab9bc0ed78db476d
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-09 16:14:42 -0300

    modifying the code

--- src/middlewares/Authentication.js
+++ src/middlewares/Authentication.js
@@ -1,27 +1,30 @@
 /* eslint-disable eqeqeq */
 import 'dotenv/config';
 import jwt from 'jsonwebtoken';
+import User from '../models/User';
 
 class Authentication {
   async auth(req, res, next) {
-    const authToken = req.headers.authorization;
-    if (authToken != undefined) {
-      const [, token] = authToken.split(' ');
-      if (token != 'undefined') {
-        await jwt.verify(token, process.env.SECRET, (err, data) => {
-          if (!err) {
-            req.idUser = data.id;
-            req.emailUser = data.email;
-            next();
-          } else {
-            res.status(401).json({ error: ['Invalid Token'] });
+    try {
+      const authToken = req.headers.authorization;
+      if (authToken != undefined) {
+        const [, token] = authToken.split(' ');
+        if (token != 'undefined') {
+          const data = jwt.verify(token, process.env.SECRET);
+          const { id, email } = data;
+          req.idUser = id;
+          req.emailUser = email;
+          const user = await User.findOne({ where: { email } });
+          if (!user) {
+            return res.status(401).json({ error: ['Not found User'] });
           }
-        });
-      } else {
-        res.status(401).json({ error: ['No Token'] });
+          return next();
+        }
+        return res.status(401).json({ error: ['No Token'] });
       }
-    } else {
-      res.status(401).json({ error: ['No Token'] });
+      return res.status(401).json({ error: ['No Token'] });
+    } catch (e) {
+      return res.status(404).json({ error: ['Not found User or Badly formatted token'] });
     }
   }
 }
