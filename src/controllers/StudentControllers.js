da2257382f33f11e4af1751b841a0b11b0dba6b8
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-10 10:44:19 -0300

    CRUD of Student

--- src/controllers/StudentControllers.js
+++ src/controllers/StudentControllers.js
@@ -1,3 +1,4 @@
+import validate from 'validator';
 import Student from '../models/Student';
 
 // eslint-disable-next-line no-unused-vars
@@ -5,11 +6,62 @@ class StudentControllers {
   async create(req, res) {
     try {
       const student = await Student.create(req.body);
-      res.status(200).json({
+      return res.status(200).json({
         student,
       });
     } catch (e) {
-      res.status(400).json({ errors: e.errors.map((err) => err.message) });
+      return res.status(400).json({ errors: e.errors.map((err) => err.message) });
+    }
+  }
+
+  async index(req, res) {
+    try {
+      const students = await Student.findAll({ attributes: ['id', 'name', 'surname', 'email', 'age', 'height', 'weight'] });
+      return res.status(200).json({ students });
+    } catch (e) {
+      return res.status(400).json({ error: ['error loading data'] });
+    }
+  }
+
+  async show(req, res) {
+    try {
+      if (isNaN(req.params.id)) return res.status(400).json({ error: ['ID invalid'] });
+      const student = await Student.findByPk(req.params.id);
+      if (!student) return res.status(404).json({ error: ['Student not found'] });
+      const {
+        id, name, surname, email, age, height, weight,
+      } = student;
+      return res.status(200).json({
+        student: {
+          id, name, surname, email, age, height, weight,
+        },
+      });
+    } catch (e) {
+      return res.status(501).json({ error: ['error loading data'] });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      if (isNaN(req.params.id)) return res.status(400).json({ error: ['ID invalid'] });
+      const student = await Student.findByPk(req.params.id);
+      if (!student) return res.status(404).json({ error: ['Student not found'] });
+      await student.update(req.body);
+      return res.status(200).json({ status: ['Updated Data'] });
+    } catch (e) {
+      return res.status(400).json({ error: e.errors.map((err) => err.message) });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      if (isNaN(req.params.id)) return res.status(400).json({ error: ['ID invalid'] });
+      const student = await Student.findByPk(req.params.id);
+      if (!student) return res.status(404).json({ error: ['Student not found'] });
+      await student.destroy();
+      return res.status(200).json({ status: ['Deleted Data'] });
+    } catch (e) {
+      return res.status(501).json({ error: ['error loading data'] });
     }
   }
 }
