4e40cba2c0809d31269a4a712495743f2c061345
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-10 10:44:56 -0300

    router of students

--- src/routes/student.routes.js
+++ src/routes/student.routes.js
@@ -4,6 +4,10 @@ import authentication from '../middlewares/Authentication';
 
 const routes = new Router();
 
+routes.get('/', authentication, StudentControllers.index);
+routes.get('/:id', authentication, StudentControllers.show);
 routes.post('/', authentication, StudentControllers.create);
+routes.put('/:id', authentication, StudentControllers.update);
+routes.delete('/:id', authentication, StudentControllers.delete);
 
 export default routes;
