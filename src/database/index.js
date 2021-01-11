312a494d5967ed3db8fefe0fe9ce7ec857526053
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-11 14:07:22 -0300

    Model Photo

--- src/database/index.js
+++ src/database/index.js
@@ -3,11 +3,13 @@ import databaseConfig from '../config/database';
 import Student from '../models/Student';
 import Report from '../models/Report';
 import User from '../models/User';
+import Photo from '../models/Photo';
 
-const models = [Student, Report, User];
+const models = [Student, Report, User, Photo];
 
 const connection = new Sequelize(databaseConfig);
 
 models.forEach((model) => model.init(connection));
 Student.associate(connection.models);
 Report.associate(connection.models);
+Photo.associate(connection.models);
