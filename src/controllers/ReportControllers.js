03345acff23e50a3a17087d587d96f15952b7088
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-11 09:27:54 -0300

    CRUD report

--- src/controllers/ReportControllers.js
+++ src/controllers/ReportControllers.js
@@ -10,56 +10,52 @@ class ReportControllers {
       if (!student) return res.status(404).json({ error: ['Student not found'] });
       req.body.student_id = req.params.id;
       const report = await Report.create(req.body);
-      res.status(200).json({ report });
+      return res.status(200).json({ report });
     } catch (e) {
-      res.status(400).json({ error: e.errors.map((err) => err.message) });
+      return res.status(400).json({ error: e.errors.map((err) => err.message) });
     }
   }
 
   async index(req, res) {
     try {
-      const reports = await Report.findAll({ include: { association: 'student' } });
-      if (!reports.length > 0) return res.status(400).json({ error: ['Not found Report'] });
-      const reportsStudent = reports.map((report) => ({
-        matter: report.matter,
-        average: report.average,
-        student: {
-          id: report.student.id,
-          name: report.student.name,
-          surname: report.student.surname,
-          email: report.student.email,
-          age: report.student.age,
-          height: report.student.height,
-          weight: report.student.weight,
-        },
-      }));
-      return res.status(200).json({ reportsStudent });
+      if (isNaN(req.params.id)) return res.status(400).json({ error: ['ID invalid'] });
+      const studentReports = await Student.findByPk(req.params.id, {
+        include: { association: 'reports', attributes: ['id', 'matter', 'note_1', 'note_2', 'note_3', 'note_4', 'average'] },
+        attributes: ['id', 'name', 'surname', 'email', 'age'],
+      });
+      if (!studentReports) return res.status(404).json({ error: ['Student not found'] });
+
+      return res.status(200).json({ studentReports });
     } catch (e) {
-      return res.status(400).json({ error: ['error loading data'] });
+      return res.status(400).json({ error: ['Error loading data'] });
     }
   }
 
-  async show(req, res) {
+  async update(req, res) {
     try {
-      if (isNaN(req.params.id)) return res.status(400).json({ error: ['ID invalid'] });
-      const studentReport = await Report.findAll({ where: { student_id: req.params.id }, include: { association: 'student' } });
-      if (!studentReport.length > 0) return res.status(400).json({ error: ['Not found Report'] });
-      const reportsStudent = studentReport.map((report) => ({
-        matter: report.matter,
-        average: report.average,
-        student: {
-          id: report.student.id,
-          name: report.student.name,
-          surname: report.student.surname,
-          email: report.student.email,
-          age: report.student.age,
-          height: report.student.height,
-          weight: report.student.weight,
-        },
-      }));
-      return res.status(200).json({ reportsStudent });
+      if (isNaN(req.params.id) || isNaN(req.params.idMatter)) return res.status(400).json({ error: ['ID invalid'] });
+      const student = await Student.findByPk(req.params.id);
+      if (!student) return res.status(404).json({ error: ['Student not found'] });
+      const report = await Report.findByPk(req.params.idMatter);
+      if (!report) return res.status(404).json({ error: ['Matter not found'] });
+      await report.update(req.body);
+      return res.status(200).json({ status: ['Updated Data'] });
     } catch (e) {
-      return res.status(400).json({ error: ['error loading data'] });
+      return res.status(400).json({ error: e.errors.map((err) => err.message) });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      if (isNaN(req.params.id) || isNaN(req.params.idMatter)) return res.status(400).json({ error: ['ID invalid'] });
+      const student = await Student.findByPk(req.params.id);
+      if (!student) return res.status(404).json({ error: ['Student not found'] });
+      const report = await Report.findByPk(req.params.idMatter);
+      if (!report) return res.status(404).json({ error: ['Matter not found'] });
+      await report.destroy();
+      return res.status(200).json({ status: ['Deleted Data'] });
+    } catch (e) {
+      return res.status(400).json({ error: ['Error loading data'] });
     }
   }
 }
