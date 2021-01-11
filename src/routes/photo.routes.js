a514883115b8932049a39e5ec9024e4c31d4c9c3
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-11 12:36:01 -0300

    middleware Image format error

--- src/routes/photo.routes.js
+++ src/routes/photo.routes.js
@@ -3,10 +3,11 @@ import multer from 'multer';
 import PhotoControllers from '../controllers/PhotoControllers';
 import authentication from '../middlewares/Authentication';
 import multerConfig from '../config/multerConfig';
+import HandlingError from '../middlewares/HandlingError';
 
 const routes = new Router();
 
 routes.use(authentication);
-routes.post('/', multer(multerConfig).single('photo'), PhotoControllers.store);
+routes.post('/', multer(multerConfig).single('photo'), HandlingError, PhotoControllers.store);
 
 export default routes;
