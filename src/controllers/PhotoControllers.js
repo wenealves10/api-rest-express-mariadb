67f6dcef2009cc5005cbb37e497c5c11f9635222
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-11 15:59:56 -0300

    C.U.D

--- src/controllers/PhotoControllers.js
+++ src/controllers/PhotoControllers.js
@@ -17,6 +17,37 @@ class PhotoControllers {
       return res.status(400).json({ error: ['Error upload Photograph'] });
     }
   }
+
+  async update(req, res) {
+    try {
+      if (isNaN(req.params.id)) return res.status(400).json({ error: ['ID invalid'] });
+      const { originalname, filename } = req.file;
+      if (!(originalname && filename)) return res.status(400).json({ error: ['Error with the image'] });
+      const student = await Student.findByPk(req.params.id);
+      if (!student) return res.status(404).json({ error: ['Student not found'] });
+      const id_student = req.params.id;
+      const photoStudent = await Photo.findOne({ where: { id_student } });
+      if (!photoStudent) return res.status(404).json({ error: ['Photograph not found'] });
+      await photoStudent.update({ originalname, filename });
+      return res.status(200).json({ status: ['Updated Photograph profile'] });
+    } catch (e) {
+      return res.status(400).json({ error: ['Error upload Photograph'] });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      if (isNaN(req.params.id)) return res.status(400).json({ error: ['ID invalid'] });
+      const student = await Student.findByPk(req.params.id);
+      if (!student) return res.status(404).json({ error: ['Student not found'] });
+      const photoStudent = await Photo.findOne({ where: { id_student: req.params.id } });
+      if (!photoStudent) return res.status(404).json({ error: ['Photograph not found'] });
+      await photoStudent.destroy();
+      return res.status(200).json({ status: ['Deleted Photograph'] });
+    } catch (e) {
+      return res.status(400).json({ error: ['Error at deleted photograph'] });
+    }
+  }
 }
 
 export default new PhotoControllers();
