5c4d0aed5ac6a3354b4a35bb9f3ea20a4bf3c182
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-20 08:35:19 -0300

    Adding helmet for safety

--- src/app/app.js
+++ src/app/app.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import helmet from 'helmet';
 import { resolve } from 'path';
 
 // databases
@@ -27,6 +28,7 @@ class App {
     this.app.use(bodyParser.urlencoded({ extended: false }));
     this.app.use(bodyParser.json());
     this.app.use(cors());
+    this.app.use(helmet());
     this.app.use(express.static(resolve(__dirname, '..', '..', 'uploads')));
   }
 
