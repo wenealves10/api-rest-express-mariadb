b1e1c0943193f0f66b651c9672fcef454e7c9b64
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-31 11:29:16 -0300

    modify delay for 150ms

--- src/app/app.js
+++ src/app/app.js
@@ -46,7 +46,7 @@ class App {
     this.app.use(bodyParser.json());
     this.app.use(cors(corsOptions));
     this.app.use(helmet());
-    this.app.use(delay(2000));
+    this.app.use(delay(150));
     this.app.use(express.static(resolve(__dirname, '..', '..', 'uploads')));
   }
 
