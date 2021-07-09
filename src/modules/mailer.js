fbe58573e59b1f1f5f5f98a941e6fc99a52b4b3f
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-07-09 10:37:28 -0300

    New service e-mail 💯

--- src/modules/mailer.js
+++ src/modules/mailer.js
@@ -2,11 +2,10 @@ import 'dotenv/config';
 import nodemailer from 'nodemailer';
 
 const transport = nodemailer.createTransport({
-  host: process.env.HOST_MAIL,
-  port: process.env.PORT_MAIL,
+  service: "gmail",
   auth: {
-    user: process.env.USER_MAIL,
-    pass: process.env.PASSWORD_MAIL,
+    user: process.env.LOGIN_EMAIL,
+    pass: process.env.LOGIN_EMAIL_PASSWORD,
   },
 });
 
